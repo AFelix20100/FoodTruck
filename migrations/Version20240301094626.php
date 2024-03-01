1c04e9380acaa687dda34297635c1843f4f365e2
ARTHUR Félix <arthur.felix160@gmail.com>
2024-03-01 11:08:28 +0100

    remove import:map

--- migrations/Version20240301094626.php
+++ migrations/Version20240301094626.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240229082810 extends AbstractMigration
+final class Version20240301094626 extends AbstractMigration
 {
     public function getDescription(): string
     {
